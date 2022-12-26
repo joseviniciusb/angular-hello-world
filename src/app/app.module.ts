299df1f42acfeef63316658480b1fbc5a0c00aca
joseviniciusb <josevbluz@outlook.com>
2022-12-26 16:18:00 -0300

    Adicionar novo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { CounterButtomComponent } from './counter-buttom/counter-buttom.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterButtomComponent
+    CounterButtomComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
